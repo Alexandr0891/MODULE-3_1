6d48819db90b859b22bfe4f836670458332392f9
Alexandr0891 <alexmalcev1401@gmail.com>
2024-08-30 15:26:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,5 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-#"Функции в Python.Функция с параметром"
-def get_matrix( n, m, value):
-    matrix = []
-    for i in range(n):
-        matrix.append([n])
-        for j in range(m):
-            matrix.append([value])
-    print(matrix)
 
 
-result1 = get_matrix(2, 3, 10)
-result2 = get_matrix(3, 4, 25)
-result3 = get_matrix(4, 1, 55)
